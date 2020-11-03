cd79ff6b1b3b222fa5f90f7d5b6e340acce1305c
Gabriel Duque <gabriel@zuh0.com>
2020-11-03 12:39:25 +0100

    src: sleep for SleepTime instead of exiting as batch job
    Signed-off-by: Gabriel Duque <gabriel@zuh0.com>

--- cmd/zorua/main.go
+++ cmd/zorua/main.go
@@ -12,6 +12,7 @@ import (
 )
 
 type configuration struct {
+	SleepTime int64
 	Domain      string
 	Credentials struct {
 		Username string
@@ -24,55 +25,61 @@ func readConfig(filename string) (configuration configuration) {
 	/* Read the whole file into a buffer */
 	content, err := ioutil.ReadFile(filename)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 
 	/* Parse and validate JSON */
 	if err := json.Unmarshal(content, &configuration); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	} else if configuration.Domain == "" {
-		log.Fatal("Missing or empty 'Domain' field in JSON configuration.")
+		log.Println("Missing or empty 'Domain' field in JSON configuration.")
 	} else if configuration.Credentials.Username == "" {
-		log.Fatal("Missing or empty 'Credentials.Username' field in JSON configuration.")
+		log.Println("Missing or empty 'Credentials.Username' field in JSON configuration.")
 	} else if configuration.Credentials.Password == "" {
-		log.Fatal("Missing or empty 'Credentials.Password' field in JSON configuration.")
+		log.Println("Missing or empty 'Credentials.Password' field in JSON configuration.")
+	} else if configuration.SleepTime == 0 {
+		configuration.SleepTime = 5
 	}
 
 	return
 }
 
 /* Get our public IP address record by fetching `https://domains.google.com/checkip` */
-func getCurrentIP() (ip net.IP) {
+func getCurrentIP() (ip net.IP, err error) {
 	/* Get ipv4 */
 	response, err := http.Get("https://api.ipify.org")
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 
 	/* Make sure we check closing errors */
 	defer func(response *http.Response) {
 		if err := response.Body.Close(); err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
 	}(response)
 
 	ipBytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal(err)
-	}
-	err = ip.UnmarshalText(ipBytes)
-	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 
+	err = ip.UnmarshalText(ipBytes)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 	return
 }
 
 /* Check if we need to update the record */
-func needsUpdate(domain string, ip net.IP) (update bool) {
+func needsUpdate(domain string, ip net.IP) (update bool, err error) {
 	ips, err := net.LookupIP(domain)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 
 	for _, currIp := range ips {
@@ -85,7 +92,7 @@ func needsUpdate(domain string, ip net.IP) (update bool) {
 }
 
 /* This updates the record for the given IP address using the given configuration */
-func updateRecord(configuration configuration, ip net.IP) {
+func updateRecord(configuration configuration, ip net.IP) (err error) {
 	url := fmt.Sprintf("https://domains.google.com/nic/update?hostname=%v&myip=%v", configuration.Domain, ip)
 	log.Println("Query URL for update:", url)
 
@@ -96,19 +103,21 @@ func updateRecord(configuration configuration, ip net.IP) {
 
 	request, err := http.NewRequest("POST", url, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	request.SetBasicAuth(configuration.Credentials.Username, configuration.Credentials.Password)
 
 	response, err := client.Do(request)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 
 	/* Make sure we check closing errors */
 	defer func(response *http.Response) {
 		if err := response.Body.Close(); err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
 	}(response)
 
@@ -116,12 +125,38 @@ func updateRecord(configuration configuration, ip net.IP) {
 	strBody := string(body)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	} else if strBody != fmt.Sprintf("good %v", ip) && strBody != fmt.Sprintf("nochg %v", ip) {
-		log.Fatal(" Response: ", strBody)
+		log.Println(" Response: ", strBody)
 	}
 
 	log.Println("Response:", strBody)
+	return
+}
+/* Do the actual magic */
+func updateHandler(configuration configuration) {
+	ipv4, err := getCurrentIP()
+	if err != nil {
+		log.Println("Could not get public IP waiting for next run")
+		return
+	}
+	log.Println("Current IPv4:", ipv4)
+
+	log.Println("Checking if we need to update our record")
+	needUpdate, err := needsUpdate(configuration.Domain, ipv4)
+	if err != nil {
+		log.Println("Couldn't check for update")
+		return
+	} else if needUpdate {
+		log.Println("Updating record")
+		if err := updateRecord(configuration, ipv4); err != nil {
+			log.Println("Failed to update our IP")
+			return
+		}
+	} else {
+		log.Println("No need to update record")
+	}
 }
 
 /* Entrypoint of our program */
@@ -132,15 +167,14 @@ func main() {
 	log.Println("Using configuration file:", *filename)
 	configuration := readConfig(*filename)
 	log.Println("Found valid configuration for domain:", configuration.Domain)
+	log.Println("Sleep time is", configuration.SleepTime, "minute(s)")
 
-	ipv4 := getCurrentIP()
-	log.Println("Current IPv4:", ipv4)
+	updateHandler(configuration)
+	log.Println("Sleeping for", configuration.SleepTime, "minute(s)")
 
-	log.Println("Checking if we need to update our record")
-	if needsUpdate(configuration.Domain, ipv4) {
-		log.Println("Updating record")
-		updateRecord(configuration, ipv4)
-	} else {
-		log.Println("No need to update record")
+	for _ = range time.NewTicker(time.Duration(configuration.SleepTime) * time.Minute).C {
+		log.Println("Waking up")
+		updateHandler(configuration)
+		log.Println("Sleeping for", configuration.SleepTime, "minute(s)")
 	}
 }
